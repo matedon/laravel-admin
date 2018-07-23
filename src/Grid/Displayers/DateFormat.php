15591750fe0903e91c3909591818c53c36376452
kotroczo.mate <kotroczo.mate@gmail.com>
2018-07-23 09:30:43 +0200

    DateFormat return empty string if input is not a date or null.

--- src/Grid/Displayers/DateFormat.php
+++ src/Grid/Displayers/DateFormat.php
@@ -6,6 +6,9 @@ class DateFormat extends AbstractDisplayer
 {
     public function display($format = 'y-m-d<\b\r>H:i:s')
     {
+        if (empty(trim($this->value))) {
+            return '';
+        }
         return date($format, strtotime($this->value));
     }
 }
